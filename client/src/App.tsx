5c8ee851f1e864ceada5d941ef776d47a80a3f71
Cajsa Nummelin <cajsa.nummelin@gmail.com>
2024-09-02 16:38:34 +0300

    Move logic for making deposits to separate component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,39 +1,15 @@
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import { getBalance, updateBalance } from "./requests/balance";
+import { useQuery } from "@tanstack/react-query";
+import { getBalance } from "./requests/balance";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import { Button, TextField } from "@mui/material";
-import { useState } from "react";
+import { NewDeposit } from "./components/NewDeposit";
 
 function App() {
-  const queryClient = useQueryClient();
-  const [amount, setAmount] = useState<string>("");
 
   const result = useQuery({
     queryKey: ["balance"],
     queryFn: getBalance,
   });
-
-  const newDepositMutation = useMutation({
-    mutationFn: updateBalance,
-    onSuccess: () => {
-      console.log("success");
-      queryClient.invalidateQueries({ queryKey: ["balance"] });
-    },
-  });
-
-  const handleNewDeposit = async (event: React.SyntheticEvent) => {
-    event.preventDefault();
-    if (Number(amount) && Number(amount) > 0) {
-      await newDepositMutation.mutateAsync({
-        amount: Number(amount),
-        action: "add",
-      });
-    } else {
-      window.alert("Amount must be a valid number with no special characters");
-    }
-    setAmount("")
-  };
 
   if (result.isLoading) {
     return <div>loading data...</div>;
@@ -59,28 +35,7 @@ function App() {
           </Box>
         </div>
         <div>
-          <form onSubmit={handleNewDeposit}>
-            <TextField
-              label="Enter amount"
-              variant="outlined"
-              value={amount}
-              onChange={(e) => setAmount(e.target.value)}
-              fullWidth
-              required
-              sx={{
-                borderRadius: "10px",
-                backgroundColor: "#f5f5f5",
-              }}
-            />
-            <Button
-              type="submit"
-              variant="contained"
-              color="primary"
-              sx={{ mt: 2 }}
-            >
-              Submit
-            </Button>
-          </form>
+          <NewDeposit />
         </div>
       </div>
     );
---
+++ client/src/components/NewDeposit.tsx
@@ -0,0 +1,55 @@
+import { Button, TextField } from "@mui/material";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+import { updateBalance } from "../requests/balance";
+
+export const NewDeposit = () => {
+  const queryClient = useQueryClient();
+  const [amount, setAmount] = useState<string>("");
+
+  const newDepositMutation = useMutation({
+    mutationFn: updateBalance,
+    onSuccess: () => {
+      console.log("success");
+      queryClient.invalidateQueries({ queryKey: ["balance"] });
+    },
+  });
+  const handleNewDeposit = async (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    if (Number(amount) && Number(amount) > 0) {
+      await newDepositMutation.mutateAsync({
+        amount: Number(amount),
+        action: "add",
+      });
+    } else {
+      window.alert("Amount must be a valid number with no special characters");
+    }
+    setAmount("");
+  };
+  return (
+    <div>
+      <form onSubmit={handleNewDeposit}>
+        <TextField
+          label="Enter amount"
+          variant="outlined"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+          fullWidth
+          required
+          sx={{
+            borderRadius: "10px",
+            backgroundColor: "#f5f5f5",
+          }}
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          sx={{ mt: 2 }}
+        >
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
+};
